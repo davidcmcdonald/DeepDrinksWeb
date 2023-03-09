4d55ed034ffd6cfdce98ca20725f05bf2dffb918
tomanagle <tomanagle@gmail.com>
2023-03-10 07:38:57 +1000

    fix: responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,21 @@ module.exports = {
         "hero-banner": "url('banner.png')",
       },
     },
+    container: {
+      width: "100%",
+      maxWidth: {
+        DEFAULT: "100%",
+        sm: "100%",
+        lg: "1280px",
+      },
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "2rem",
+        xl: "3rem",
+        "2xl": "3rem",
+      },
+    },
   },
   plugins: [],
 };
