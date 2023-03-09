e2ff19b91fd6205367a8dceac7419fe49ea2f653
tomanagle <tomanagle@gmail.com>
2023-03-10 07:12:13 +1000

    fix: encoding

---
+++ .vercel/output/static/entry.mjs
@@ -0,0 +1,8 @@
+import { s as server_default } from './chunks/astro.773ac2fc.mjs';
+import { _ as _page0, a as _page1 } from './chunks/pages/all.87473adf.mjs';
+/* empty css                                      */import 'zod';
+
+const pageMap = new Map([["src/pages/index.astro", _page0],["src/pages/disclaimer.md", _page1],]);
+const renderers = [Object.assign({"name":"astro:jsx","serverEntrypoint":"astro/jsx/server.js","jsxImportSource":"astro"}, { ssr: server_default }),];
+
+export { pageMap, renderers };
